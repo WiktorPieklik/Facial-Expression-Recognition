4e880cb6091820a8c7b8103e1fa9b346465e7ea1
WiktorPieklik <wiktor.pieklik@icloud.com>
2022-12-01 09:39:50 +0100

    fix(paths)!: root dir compatible with jupyter notebooks

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+import os
 
-ROOT_DIR = Path(__file__).parent.parent
+ROOT_DIR = Path(os.path.abspath('')).parent.parent
 RAW_DIR = ROOT_DIR / "raw"
 IMG_DIR = RAW_DIR / "images"
 IMG_SHAPE = (48, 48)
